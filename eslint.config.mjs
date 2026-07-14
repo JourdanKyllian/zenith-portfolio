5482c762b375346c0d5618a117919577cb0fef87
KyllianDev <kyllianjourdan@outlook.com>
2026-07-14 11:15:25 +0200

    style: désactivation globale de la règle no-img-element pour les images externes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Désactivation globale de la règle sur les balises <img>
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
